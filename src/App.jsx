a391089f29743abe9bf7c0c48e3e146bb70810c8
franklin-sula <franklin.sula09@gmail.com>
2024-11-14 15:06:54 +0800

    Initial Home Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,7 @@
-import { useState } from "react";
+import Home from "./pages/Home";
 
 function App() {
-  return (
-    <div className=" flex justify-center items-center h-screen">
-      <p className=" text-4xl font-bold text-accent font-montserrat">
-        Project Uroboros V2
-      </p>
-    </div>
-  );
+  return <Home />;
 }
 
 export default App;
