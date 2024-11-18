a923791d72a83d71ccf7a9def57d0d856baef469
franklin-sula <franklin.sula09@gmail.com>
2024-11-18 10:58:15 +0800

    Added buttons in Home

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,5 @@
-import React from 'react'
+import React from "react";
 
 export default function Dashboard() {
-  return (
-    <div className=' text-heading text-accent font-bold '>Dashboard</div>
-  )
+  return <div className=" text-heading text-accent font-bold ">Dashboard</div>;
 }
