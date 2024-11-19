edec8e06dec8bc1793e20d73ec216bf00a494a1e
A2K Group International <a2kdev@a2kgroup.co.uk>
2024-11-19 17:14:31 +0800

    Added dropdown in input event

--- src/components/Home/FamilyRegistration.jsx
+++ src/components/Home/FamilyRegistration.jsx
@@ -11,7 +11,7 @@ import {
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { addFamilySchema } from "@/zodSchema/AddFamilySchema";
-import { Label } from "../ui/label";
+import { Label } from "@/components/ui/label";
 import { useToast } from "@/hooks/use-toast";
 import { DialogClose } from "../ui/dialog";
 
