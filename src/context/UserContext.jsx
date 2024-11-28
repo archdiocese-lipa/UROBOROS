039ed5ec6af110f6e607b8faf8fd7e20be6c517b
A2KMarkie <mark.liwanag@a2kgroup.org>
2024-11-28 15:04:50 +0800

    admin can login force

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -10,6 +10,7 @@ export const UserProvider = ({ children }) => {
   const [loading, setLoading] = useState(false);
 
   // Login function
+  // Login function
   const login = async (credentials) => {
     setLoading(true);
     try {
@@ -26,8 +27,8 @@ export const UserProvider = ({ children }) => {
 
       if (fetchError) throw fetchError;
 
-      // Check if the user's account is confirmed
-      if (!fullUser.is_confirmed) {
+      // Allow login if the user's role is "admin" or if their account is confirmed
+      if (!fullUser.is_confirmed && fullUser.role !== "admin") {
         throw new Error(
           "Your account is not confirmed. Please contact an admin"
         );
