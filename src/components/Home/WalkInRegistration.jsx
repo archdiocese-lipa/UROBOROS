a338b561a335ee7d94d89edf275963fa5b0f2155
A2K Group International <a2kdev@a2kgroup.co.uk>
2024-12-11 17:33:56 +0800

    Fix naming convention for registration
    -Change Parent/Guardian Information to Parent/Guardian
    -Change Child Information to Children

--- src/components/Home/WalkInRegistration.jsx
+++ src/components/Home/WalkInRegistration.jsx
@@ -231,7 +231,7 @@ const WalkInRegistration = () => {
                   )}
                 />
                 {/* Parent Guardian Field */}
-                <Label className="text-lg">Parent/Guardian Information</Label>
+                <Label className="text-lg">Parent/Guardian</Label>
                 <span className="hidden text-sm italic text-zinc-400 md:block">
                   (Check the box on the left to choose the main applicant).
                 </span>
@@ -331,7 +331,7 @@ const WalkInRegistration = () => {
                     Add
                   </Button>
                 </div>
-                <Label className="text-lg">Child Information </Label>
+                <Label className="text-lg">Children</Label>
                 {childFields.map((field, index) => (
                   <div
                     key={field.id}
