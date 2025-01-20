6063fe4d3b492dc95c07865dffc6721f2f1f4f23
Tristan Santos <115911660+TristanJasperSantos@users.noreply.github.com>
2025-01-20 14:01:41 +0800

    Added tooltip and search in attendance

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { createRoot } from 'react-dom/client';
 import {  QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'; // Import Devtools
 import { Toaster } from '@/components/ui/toaster';
+import { TooltipProvider } from '@radix-ui/react-tooltip';
 import App from '@/App';
 import '@/index.css';
 import '@/lib/prototypes'; // From the main branch
@@ -20,11 +21,13 @@ const root = createRoot(rootElement);
 root.render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
       <UserProvider>
         {/* Wrap the app in UserProvider */}
         <App />
         <Toaster />
       </UserProvider>
+      </TooltipProvider>
       {/* Add ReactQueryDevtools for debugging */}
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
