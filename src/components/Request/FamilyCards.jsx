7b35edf1fb28759c1e38108126de680e9c9e2b3c
franklin-sula <franklin.sula09@gmail.com>
2025-01-31 14:59:35 +0800

    Update FamilyCards.jsx

--- src/components/Request/FamilyCards.jsx
+++ src/components/Request/FamilyCards.jsx
@@ -19,7 +19,7 @@ import {
 } from "../ui/table";
 import { cn } from "@/lib/utils";
 import { Input } from "../ui/input";
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
 import { useDebounce } from "@/hooks/useDebounce";
 import { Search } from "@/assets/icons/icons";
 
@@ -48,7 +48,7 @@ const FamilyCards = () => {
       const response = await fetchFamilies({
         page: pageParam,
         pageSize: 10,
-        search
+        search,
       });
 
       return response;
@@ -72,53 +72,54 @@ const FamilyCards = () => {
     return <div>Error loading families.</div>;
   }
 
-
-
   return (
     <div className="flex flex-col gap-2">
-      <div className=" w-full flex justify-center items-center">
-      <div className="  relative w-8/12">
-        <Search className="absolute left-4 top-1/2 -translate-y-1/2 transform text-2xl text-accent" />
-        <Input
-          value={search}
-          onChange={handleSearch}
-          className="border-none pl-12"
-          placeholder="Search family"
-        />
-      </div>
+      <div className="flex w-[30rem] items-center justify-start">
+        <div className="relative w-8/12">
+          <Search className="absolute left-4 top-1/2 -translate-y-1/2 transform text-2xl text-accent" />
+          <Input
+            value={search}
+            onChange={handleSearch}
+            className="border-none pl-12"
+            placeholder="Search family"
+          />
+        </div>
       </div>
 
-      { isLoading ? <Loading/> :data?.pages?.flatMap((page) =>
-        page?.items?.map((family) => (
-          <Card key={family.id} className="p-2">
-            <CardHeader className="p-2">
-              <CardTitle className="font-montserrat font-bold text-accent">
-              {family?.users?.first_name} {family?.users?.last_name} Family
-              </CardTitle>
-              <CardDescription className="sr-only">
-                Family Details
-              </CardDescription>
-            </CardHeader>
-            <CardContent className="flex flex-col gap-3 p-1">
-              {/* Parent(s) */}
-              <div className="flex justify-between">
-                <div className="flex items-center gap-2">
-                  <h3 className="text-xl font-semibold text-accent">
-                    Parent(s)/ Guardian(s)
-                  </h3>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        data?.pages?.flatMap((page) =>
+          page?.items?.map((family) => (
+            <Card key={family.id} className="p-2">
+              <CardHeader className="p-2">
+                <CardTitle className="font-montserrat font-bold text-accent">
+                  {family?.users?.first_name} {family?.users?.last_name} Family
+                </CardTitle>
+                <CardDescription className="sr-only">
+                  Family Details
+                </CardDescription>
+              </CardHeader>
+              <CardContent className="flex flex-col gap-3 p-1">
+                {/* Parent(s) */}
+                <div className="flex justify-between">
+                  <div className="flex items-center gap-2">
+                    <h3 className="text-xl font-semibold text-accent">
+                      Parent(s)/ Guardian(s)
+                    </h3>
+                  </div>
                 </div>
-              </div>
-              {family.parents && (
-                <Table>
-                  <TableHeader className="bg-primary">
-                    <TableRow>
-                      <TableHead>Name</TableHead>
-                      <TableHead>Contact</TableHead>
-                    </TableRow>
-                  </TableHeader>
+                {family.parents && (
+                  <Table>
+                    <TableHeader className="bg-primary">
+                      <TableRow>
+                        <TableHead>Name</TableHead>
+                        <TableHead>Contact</TableHead>
+                      </TableRow>
+                    </TableHeader>
 
-                  <TableBody>
-                    {/* {family.parents.length < 1 && (
+                    <TableBody>
+                      {/* {family.parents.length < 1 && (
                       <TableRow>
                         <TableCell colSpan={6} className="text-center">
                           <div className="flex w-full items-center justify-center">
@@ -127,7 +128,7 @@ const FamilyCards = () => {
                         </TableCell>
                       </TableRow>
                     )} */}
-                    {/* <TableRow>
+                      {/* <TableRow>
                       <TableCell>
                         <p>{`${family?.users?.first_name} ${family?.users?.last_name}`}</p>
                       </TableCell>
@@ -135,65 +136,72 @@ const FamilyCards = () => {
                         <p>{`${family?.users?.contact_number}`}</p>
                       </TableCell>
                     </TableRow> */}
-                    {family?.parents?.map((parent, i) => (
-                      <TableRow
-                        key={i}
-                        className={cn(
-                          i % 2 !== 0 ? "bg-primary bg-opacity-35" : "bg-white"
-                        )}
-                      >
-                        <TableCell className="text-nowrap py-1 md:p-4">
-                          <p>{`${parent.first_name} ${parent.last_name}`}</p>
-                        </TableCell>
-                        <TableCell className="text-nowrap py-1 md:p-4">
-                          <p>{`${parent.contact_number}`}</p>
-                        </TableCell>
-                      </TableRow>
-                    ))}
-                  </TableBody>
-                </Table>
-              )}
+                      {family?.parents?.map((parent, i) => (
+                        <TableRow
+                          key={i}
+                          className={cn(
+                            i % 2 !== 0
+                              ? "bg-primary bg-opacity-35"
+                              : "bg-white"
+                          )}
+                        >
+                          <TableCell className="text-nowrap py-1 md:p-4">
+                            <p>{`${parent.first_name} ${parent.last_name}`}</p>
+                          </TableCell>
+                          <TableCell className="text-nowrap py-1 md:p-4">
+                            <p>{`${parent.contact_number}`}</p>
+                          </TableCell>
+                        </TableRow>
+                      ))}
+                    </TableBody>
+                  </Table>
+                )}
 
-              {/* Children */}
-              <div className="flex items-center gap-2">
-                <h3 className="text-xl font-semibold text-accent">Children</h3>
-              </div>
-              {family?.children && (
-                <Table>
-                  <TableHeader className="bg-primary">
-                    <TableRow>
-                      <TableHead>Name</TableHead>
-                    </TableRow>
-                  </TableHeader>
-
-                  <TableBody>
-                    {family?.children.length < 1 && (
+                {/* Children */}
+                <div className="flex items-center gap-2">
+                  <h3 className="text-xl font-semibold text-accent">
+                    Children
+                  </h3>
+                </div>
+                {family?.children && (
+                  <Table>
+                    <TableHeader className="bg-primary">
                       <TableRow>
-                        <TableCell colSpan={6} className="text-center">
-                          <div className="flex w-full items-center justify-center">
-                            <p>No data found.</p>
-                          </div>
-                        </TableCell>
+                        <TableHead>Name</TableHead>
                       </TableRow>
-                    )}
-                    {family?.children.map((child, i) => (
-                      <TableRow
-                        key={i}
-                        className={cn(
-                          i % 2 !== 0 ? "bg-primary bg-opacity-35" : "bg-white"
-                        )}
-                      >
-                        <TableCell className="text-nowrap py-1 md:p-4">
-                          <p>{`${child.first_name} ${child.last_name}`}</p>
-                        </TableCell>
-                      </TableRow>
-                    ))}
-                  </TableBody>
-                </Table>
-              )}
-            </CardContent>
-          </Card>
-        ))
+                    </TableHeader>
+
+                    <TableBody>
+                      {family?.children.length < 1 && (
+                        <TableRow>
+                          <TableCell colSpan={6} className="text-center">
+                            <div className="flex w-full items-center justify-center">
+                              <p>No data found.</p>
+                            </div>
+                          </TableCell>
+                        </TableRow>
+                      )}
+                      {family?.children.map((child, i) => (
+                        <TableRow
+                          key={i}
+                          className={cn(
+                            i % 2 !== 0
+                              ? "bg-primary bg-opacity-35"
+                              : "bg-white"
+                          )}
+                        >
+                          <TableCell className="text-nowrap py-1 md:p-4">
+                            <p>{`${child.first_name} ${child.last_name}`}</p>
+                          </TableCell>
+                        </TableRow>
+                      ))}
+                    </TableBody>
+                  </Table>
+                )}
+              </CardContent>
+            </Card>
+          ))
+        )
       )}
       {/* Loading Next Page */}
       {isFetchingNextPage && <Loading />}
